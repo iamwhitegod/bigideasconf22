c2c153e6cd91ecbf2fff2c0e2a3a0d9e6e8a721b
iamwhitegod <kmbagwu12@gmail.com>
2022-10-10 07:52:21 +0100

    added organizers social links, animations, sticky nav

--- js/index.js
+++ js/index.js
@@ -1,18 +1,64 @@
+const nav = document.querySelector(".navbar");
 const navbar = document.querySelector(".navbar__nav");
 const navList = document.querySelector(".navbar__list");
 
-navbar.addEventListener("click", function (e) {
-  const elem = e.target.closest(".navbar__menu-open");
+/////////////////////////////////////////
+// Open and Close Mobile Navigation
+const menuCtrl = function (className) {
+  navbar.addEventListener("click", function (e) {
+    const elem = e.target.closest(className);
 
-  if (elem) {
-    navList.classList.toggle("open--menu");
-  }
+    if (elem) {
+      navList.classList.toggle("open--menu");
+    }
+  });
+};
+
+menuCtrl(".navbar__menu-open");
+menuCtrl(".navbar__menu-close");
+menuCtrl(".navbar__link");
+
+///////////////////////////////////////
+// Sticky navigation: Intersection Observer API
+
+const header = document.querySelector(".header");
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
 });
 
-navbar.addEventListener("click", function (e) {
-  const elem = e.target.closest(".navbar__menu-close");
+headerObserver.observe(header);
 
-  if (elem) {
-    navList.classList.toggle("open--menu");
-  }
+///////////////////////////////////////////
+// Reveal Section on Scroll
+const allSections = document.querySelectorAll("section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  threshold: 0.2,
+  root: null,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
 });
